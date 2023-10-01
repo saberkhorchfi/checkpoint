c10c6d4414e3650c445bdc63f910b521a3644c0b
unknown <khorchfisaber@gmail.com>
2023-10-01 23:27:18 +1200

    checkpoint

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import BasicExample from './BasicExample'
+import AlertDismissibleExample from './AlertDismissibleExample'
+import Partie3 from './Partie3'
+
+function App() {
+  return (
+    <div>
+      <BasicExample></BasicExample>
+      <AlertDismissibleExample></AlertDismissibleExample>
+      <Partie3></Partie3>
+      <Partie3></Partie3>
+      <Partie3></Partie3>
+      
+    </div>
+  )
+}
+
+export default App
